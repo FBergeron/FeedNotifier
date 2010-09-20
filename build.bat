0f57b77337f7220d296a0b62a4e16c1d1efc083d
FBergeron <fbergeron@users.sourceforge.net>
2010-09-20 18:58:49 +0900

    Added locale strings in dist directory.

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 python setup.py
 "C:\Program Files\Inno Setup 5\Compil32.exe" /cc installer.iss
 copy dist\revision.txt installer
+mkdir dist\locale\en\LC_MESSAGES
+mkdir dist\locale\fr\LC_MESSAGES
+copy locale\en\LC_MESSAGES\FeedNotifier.mo dist\locale\en\LC_MESSAGES
+copy locale\fr\LC_MESSAGES\FeedNotifier.mo dist\locale\fr\LC_MESSAGES
